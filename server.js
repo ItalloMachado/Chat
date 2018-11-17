ac06d549c137207d34ab4180691dcb0ae953c10c
ItalloMachado <42552075+ItalloMachado@users.noreply.github.com>
2018-11-17 17:27:40 -0200

    Update server.js
    Comentarios

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
+/*Arquivo Js do servidor do Projeto deo Chat -- Autor:Itallo Guilherme Machado*/
+//dependências do npm(precisa do Node.Js)
 const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
-const server = app.listen(3000);
+const server = app.listen(3000);//porta escolhida para a comunicação.
 const io = socket(server);
-var   usuarios=[];
+var   usuarios=[];//variável que determina o nome dos usuários online.
 
-app.use(express.static('Public'));
+app.use(express.static('Public'));// determinando o caminho do projeto.????
 
-io.sockets.on('connection', newConection);
-console.log('running...')
+io.sockets.on('connection', newConection);// Atendo para conecções dos clientes.
+console.log('running...');
 
 
 function newConection(socket){
-    console.log('Conectado: '+ socket.id+' ping: '+socket.ping);
-    socket.on('msg',recebido);
-    socket.on('user',UsuarioRecebido);
-    socket.on('disconnect',usuarioDesconectado);
+    socket.on('msg',recebido);//Escuta de mensagens dos clientes
+    socket.on('user',UsuarioRecebido);//Escuta do nome dos clientes
+    socket.on('disconnect',usuarioDesconectado);//Escuta dos clientes que desconectaram
     console.log(socket.disconnected);
     
+    //Funcao ativa quando recebe uma mensagem. A função pega a mensagem e o nome do usuario que mandou a mensagem 
+    //recebida e distribui para todos os clientes conectados.
     function recebido(data){
         socket.broadcast.emit('msgRecebido',{msg: data, nome: socket.username});
-        console.log(socket.username);
-        console.log(data);
     }
+    //funcao ativa quando há um novo usuario. Ela atualiza a array de usuarios adicionando o nome do novo cliente.
     function UsuarioRecebido(data){
         socket.username=data;
-        console.log(data);
-        console.log(socket.username);
         usuarios.push(socket.username);
         console.log("Usuarios"+usuarios);
         UpdateUsuarios();
     }
+    //Funcao ativa quando um cliente é desconectado. Ele atualiza a array de usuários retirando o nome 
+    //do cliente desconectado.
     function usuarioDesconectado(){
         console.log('Usuario desconectado');
         console.log(socket.username);
         usuarios.splice(usuarios.indexOf(socket.username),1);
         UpdateUsuarios();
     }
+    //Funcao que  atualiza a lista de cliente conectado no servidor. Ele manda o nome de todos os 
+    //cliente para todos clientes conectados. 
     function UpdateUsuarios(){
         socket.emit('UpdateUsuarios',usuarios);
         socket.broadcast.emit('UpdateUsuarios',usuarios);
